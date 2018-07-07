1ac5dad253a65c095a6308abc57d8830f6d5ec43
royyandzakiy <royyandzakiy@students.itb.ac.id>
2018-07-07 07:08:40 +0700

    first commit

---
+++ app/src/main/java/jaballogian/bluetoothtest/SplashScreen.java
@@ -0,0 +1,36 @@
+package jaballogian.bluetoothtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(700);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashScreen.this,DeviceList.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+}
