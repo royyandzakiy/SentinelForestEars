6b75877ca93b263b104a75b9ff0b88d4fdac8410
royyandzakiy <royyandzakiy@students.itb.ac.id>
2018-07-20 17:49:47 +0700

    send data CLEAR

--- app/src/main/java/thesentinel/watcher/DetectedActivity.java
+++ app/src/main/java/thesentinel/watcher/DetectedActivity.java
@@ -16,11 +16,12 @@ public class DetectedActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detected);
 
         Intent i = getIntent();
-        //if (i.hasExtra("latlng")){
-        //    latlng = i.getStringExtra("latlng");
-        //} else {
+        if (i.hasExtra("latlng")){
+            Bundle j = getIntent().getExtras();
+            latlng = j.getString("latlng");
+        } else {
             latlng = "-6.9,107.8";
-        //}
+        }
         latlngValue = (TextView) findViewById(R.id.latlngValue);
         latlngValue.setText(String.valueOf(latlng));
     }
